68a5dff0c8c226f62daad440c497a48b8a78f2a6
zealotzealot <hsh0908y@gmail.com>
2018-11-05 02:29:48 +0900

    Create model Kkok

--- kkok/main/models.py
+++ kkok/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Kkok(models.Model):
+  session = models.IntegerField()
+  time = models.DateTimeField(auto_now_add = True)
+  familyId = models.IntegerField()
+
