43fdf3b5ecbee3a4cc33014046b69f5a5243affb
zealotzealot <hsh0908y@gmail.com>
2018-11-05 02:47:21 +0900

    Implement loading kkoks already made

--- kkok/main/views.py
+++ kkok/main/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from . import models
+import datetime
 
 # Create your views here.
 def productView(request, sessionId, familyId):
+  baseTime = datetime.datetime(2000, 1, 1, 0, 0)
+  kkoks = [{
+    "family_id": k.familyId,
+    "time": int((k.time.timestamp() - baseTime.timestamp()) * 1000)
+  } for k in models.Kkok.objects.filter(sessionId=sessionId)]
+
   return render(request, "Prototype/index.html", {
     "is_test": "false",
     "family_id": familyId,
+    "kkoks": kkoks,
   })
 
 
