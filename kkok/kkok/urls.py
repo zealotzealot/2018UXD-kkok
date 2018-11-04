fd0200b7890cd9d904c22745b22ad62bbd2ba398
zealotzealot <hsh0908y@gmail.com>
2018-11-05 01:56:38 +0900

    Make test page

--- kkok/kkok/urls.py
+++ kkok/kkok/urls.py
@@ -20,4 +20,5 @@ from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('product/', views.productView),
+    path('test/', views.testView),
 ]
--- kkok/main/views.py
+++ kkok/main/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def productView(request):
-  return render(request, "Prototype/index.html", {})
+  return render(request, "Prototype/index.html", {
+    "is_test": "false",
+  })
+
+
+
+def testView(request):
+  return render(request, "Prototype/index.html", {
+    "is_test": "true",
+  })
