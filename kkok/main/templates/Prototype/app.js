zealotzealot/2018UXD-kkok

commit 46b62152ad44821621fb515711c9df9058a1cebb

Performance optimization in kkok opacity

kkok/main/templates/Prototype/app.js
@@ -317,17 +317,29 @@ Utils.interval(1.0/60, function() {
 });
 
 let updateCount = 0;
-Utils.interval(1.0/10, function() {
+let UPDATE_PERIOD = 500;
+let UPDATE_GROUPS = 20;
+Utils.interval(UPDATE_PERIOD/1000, function() {
   updateCount++;
 
   for (let i=0; i<kkoks.length; i++) {
     let k = kkoks[i];
 
-    if (k.opacity<0.1 && i%5!=updateCount%5)
+    if (k.opacity<0.1 && i%UPDATE_GROUPS!=updateCount%UPDATE_GROUPS)
       continue;
 
     let timeDiff = (currentTime - k._data_time) / DAY_MILLIS * 24;
-    k.opacity = Math.max(Math.pow(0.9, timeDiff), 0.1*Math.pow(0.995, timeDiff));
+    if (k.opacity < 0.1)
+      timeDiff +=  (UPDATE_PERIOD * Math.pow(10, speedSlider.value)) / DAY_MILLIS * 24;
+    else
+      timeDiff +=  (UPDATE_PERIOD * UPDATE_GROUPS * Math.pow(10, speedSlider.value)) / DAY_MILLIS * 24;
+
+    k.animate({
+      opacity: Math.max(Math.pow(0.9, timeDiff), 0.1*Math.pow(0.995, timeDiff)),
+      options: {
+        time: UPDATE_PERIOD / 1000,
+      },
+    });
   }
   while (kkoks.length > 0) {
     if (kkoks[0].opacity < 0.01) {

commit 49c89020fc2d6cd9d11898988e9ef81c0ca37349

Fixed kkok opacity value problem caused by 46b6215

kkok/main/templates/Prototype/app.js
@@ -358,20 +358,22 @@ Utils.interval(UPDATE_PERIOD/1000, function() {
 
   for (let i=0; i<kkoks.length; i++) {
     let k = kkoks[i];
+    let nextUpdate;
 
-    if (k.opacity<0.1 && i%UPDATE_GROUPS!=updateCount%UPDATE_GROUPS)
+    if (k.opacity>=0.1)
+      nextUpdate = 1;
+    else if (i%UPDATE_GROUPS == updateCount%UPDATE_GROUPS)
+      nextUpdate = UPDATE_GROUPS;
+    else
       continue;
 
     let timeDiff = (currentTime - k._data_time) / DAY_MILLIS * 24;
-    if (k.opacity < 0.1)
-      timeDiff +=  (UPDATE_PERIOD * Math.pow(10, speedSlider.value)) / DAY_MILLIS * 24;
-    else
-      timeDiff +=  (UPDATE_PERIOD * UPDATE_GROUPS * Math.pow(10, speedSlider.value)) / DAY_MILLIS * 24;
+    timeDiff +=  (UPDATE_PERIOD * nextUpdate * Math.pow(10, speedSlider.value)) / DAY_MILLIS * 24;
 
     k.animate({
       opacity: Math.max(Math.pow(0.9, timeDiff), 0.1*Math.pow(0.995, timeDiff)),
       options: {
-        time: UPDATE_PERIOD / 1000,
+        time: UPDATE_PERIOD * nextUpdate / 1000,
       },
     });
   }